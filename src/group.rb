03f67b062783bd9e3c56286c4bc468c1862a0dcb
Eric Schulte <schulte.eric@gmail.com>
2009-12-14 11:51:38 -0700

    initial commit

---
+++ src/group.rb
@@ -0,0 +1,147 @@
+# a Group represents a connected set of boards
+#
+#  to exercise the graphing library...
+#
+#     g = Group.new
+#     g.boards = [["frffff", 7], ["frfrfff", 4], ["lfffff", 3], ["f", 9]].map{|l, v| Board.new(l, v) }
+#     g.fill_boards.size
+#     g.plot
+#     
+require 'tempfile'
+require File.join(File.dirname(__FILE__), 'board.rb')
+
+class Group
+  attr_accessor :count, :boards, :base, :maxvalue
+
+  # create a new empty group
+  def initialize(base)
+    self.count = 0
+    self.boards = []
+    self.maxvalue = 0
+    # base directory/path where images are stored
+    self.base = base
+    %x{mkdir -p #{base}}
+    %x{mkdir -p #{File.join(base, "data")}}
+  end
+
+  def fill_boards
+    # find the corners of the square bounding the points
+    xmin = 0; xmax = 0; ymin = 0; ymax = 0
+    by_cords = boards.map{ |b| b.x_y }
+    by_cords.each do |x, y|
+      xmin = x-2 if x < xmin
+      xmax = x+2 if x > xmax
+      ymin = y-2 if y < ymin
+      ymax = y+2 if y > ymax
+    end
+    # fill in every point in the square
+    (xmin..xmax).each{ |x|
+      (ymin..ymax).each{ |y|
+        self.boards << Board.new("", 0, [x, y]) unless by_cords.include?([x, y]) }  }
+    self.boards
+  end
+  
+  def rows
+    # find the corners of the square bounding the points
+    xmin = 0; xmax = 0
+    by_cords = boards.map{ |b| b.x_y }
+    by_cords.each{ |x, y| xmin = x if x < xmin; xmax = x if x > xmax }
+    # fill in every point in the square
+    (xmin..xmax)
+  end
+
+  def cols
+    # find the corners of the square bounding the points
+    ymin = 0; ymax = 0
+    by_cords = boards.map{ |b| b.x_y }
+    by_cords.each{ |x, y| ymin = y if y < ymin; ymax = y if y > ymax }
+    # fill in every point in the square
+    (ymin..ymax)
+  end
+
+  # update this group with a packet
+  def update(packet)
+    if packet.match(/^c([-\.\d]+) (.+)/)
+      new_b = Board.new($2, Float($1))
+      remaining = self.boards.reject{ |b| b.x_y == new_b.x_y }
+      self.boards = remaining << new_b
+      self.fill_boards
+    end
+  end
+
+  # fancier gnuplot graphs
+  #
+  # (gnuplot-row (col row value)
+  # 	    (setf col (+ 1 col)) (setf row (+ 1 row))
+  # 	    (format "%f  %f  %f\n%f  %f  %f\n"
+  # 		    col (- row 0.5) value ;; lower edge
+  # 		    col (+ row 0.5) value))) ;; upper edge
+  def gnuplot_row(col, row, value)
+    col += 1; row += 1
+    "#{col} #{row - 0.5} #{value}\n#{col} #{row + 0.5} #{value}\n"
+  end
+
+  # output data in a manner ingestible by gnuplot
+  #
+  # (dotimes (col num-cols)
+  #    (dotimes (row num-rows)
+  #      (setf back-edge
+  #	    (concat back-edge
+  #		    (gnuplot-row (- col 1) row (string-to-number
+  #						(nth col (nth row table))))))
+  #      (setf front-edge
+  #	    (concat front-edge
+  #		    (gnuplot-row col row (string-to-number
+  #					  (nth col (nth row table)))))))
+  #    ;; only insert once per row
+  #    (insert back-edge) (insert "\n") ;; back edge
+  #    (insert front-edge) (insert "\n") ;; front edge
+  #    (setf back-edge "") (setf front-edge ""))
+  def data()
+    self.cols.map do |col|
+      back_edge = ""; front_edge = ""
+      self.boards.select{|b| b.y == col}.sort_by{ |b| b.x }.each do |b|
+        self.maxvalue = b.value # if b.value > self.maxvalue
+        back_edge += gnuplot_row((b.y - 1), b.x, b.value)
+        front_edge += gnuplot_row(b.y, b.x, b.value)
+      end
+      back_edge + "\n" + front_edge
+    end.join("\n")
+  end
+
+  # dump my data to a temporary file
+  def data_file(counter)
+    # create a temp file
+    t = File.open(File.join(base,"data", "#{counter}.data"), 'w')
+    t << self.data
+    t.close
+    t.path
+  end
+
+  # generate a gnuplot script using techniques from
+  # http://t16web.lanl.gov/Kawano/gnuplot/plot3d2-e.html
+  def plot_script(counter = false)
+    ["set term png",
+     (if counter
+        "set output \"#{base}/#{counter}.png\"" # "set output \"#{base}/group.png\""
+      else
+        "set output \"#{base}/group.png\""
+      end),
+     "set pm3d",
+     # "set pm3d map",
+     "splot \"#{self.data_file(counter)}\" with pm3d title 'fitness' "].join("\n")
+  end
+
+  def plot(counter = false)
+    g = Tempfile.new("scrutinizer-gnuplot")
+    g << self.plot_script(counter)
+    g.flush
+    %x{gnuplot #{g.path} 2> /dev/null}
+    g.close
+    g.path
+  end
+
+  # create an animated gif of the visualizations (require imagemagick)
+  def animate() %x{convert -delay 20 -loop 1 #{base}*.png #{base}.gif} end
+
+end
